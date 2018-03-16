d0880b33797dd5b23f11e9429463adbd9f4c3cc1
Ingo Villnow <ingo@Ingos-MBP.fritz.box>
2018-03-16 18:05:14 +0100

    latest changes: GaussianNB and SVM

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,18 +15,28 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+print "training phase"
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+print "prediction phase"
+t0 = time()
+predictions = clf.predict(features_test)
+print "prediction time: ", round(time()-t0, 3), "s"
+
+print "accuracy:", clf.score(features_test, labels_test)
 
 #########################################################
 
