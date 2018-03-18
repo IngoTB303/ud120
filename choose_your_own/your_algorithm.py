e48196933269024af838cf192a5eeec073d3a2d9
Ingo Villnow <ingo@Ingos-MBP.fritz.box>
2018-03-18 11:33:45 +0100

    own algorithm implemented

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,28 +17,60 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+def initplot():
+	plt.xlim(0.0, 1.0)
+	plt.ylim(0.0, 1.0)
+	plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+	plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+	plt.legend()
+	plt.xlabel("bumpiness")
+	plt.ylabel("grade")
+	plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+def adaboost(features_train, labels_train, features_test, labels_test):
+	from sklearn.ensemble import AdaBoostClassifier
+	adaclf = AdaBoostClassifier(n_estimators=100,learning_rate=1)
+	adaclf.fit(features_train, labels_train)
+	acc = adaclf.score(features_test, labels_test)
+	print "accuracy AdaBoostClassifier: ", acc
+	plt.title('AdaBoostClassifier')
+	return adaclf
 
+def knearest(features_train, labels_train, features_test, labels_test):
+	from sklearn.neighbors import KNeighborsClassifier
+	kclf = KNeighborsClassifier(n_neighbors=7, weights='distance', algorithm='auto',
+		p=3, metric='minkowski', n_jobs=1)
+	kclf.fit(features_train, labels_train)
+	acc = kclf.score(features_test, labels_test)
+	print "accuracy NearestNeighbors: ", acc
+	plt.title('k-nearest')
+	return kclf	
 
-
-
-
-
+def randomforest(features_train, labels_train, features_test, labels_test):
+	from sklearn.ensemble import RandomForestClassifier
+	rfclf = RandomForestClassifier(n_estimators=5, criterion='gini', 
+		min_samples_split=4, min_samples_leaf=4)
+	rfclf.fit(features_train, labels_train)
+	acc = rfclf.score(features_test, labels_test)
+	print "accuracy RandomForestClassifier: ", acc
+	plt.title('RandomForestClassifier')
+	return rfclf	
 
 try:
+    #clf = adaboost(features_train, labels_train, features_test, labels_test)
+    #prettyPicture(clf, features_test, labels_test)
+    #plt.show()
+    # BEST ACCURACY:
+    clf = knearest(features_train, labels_train, features_test, labels_test)
     prettyPicture(clf, features_test, labels_test)
+    plt.show()
+    #clf = randomforest(features_train, labels_train, features_test, labels_test)
+    #prettyPicture(clf, features_test, labels_test)
+    #plt.show()
 except NameError:
     pass
